c6f4f68259ed175830472f5fe81e2a1be3e7f054
Elvin Yang <eyang@cs.utexas.edu>
2023-02-02 07:17:24 -0600

    Add build flag for initial setup

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 
 import argparse
 import subprocess
+import os
 import sys
 from typing import NoReturn
 
@@ -14,10 +15,13 @@ class ArgumentParser(argparse.ArgumentParser):
         self.exit(2, f"\n{self.prog}: error: {message}\n")
 
 
-def build(tag: str) -> None:
+def build(tag: str, with_initial_setup: bool) -> None:
     subprocess_args = ["make", "-f", f"noetic/{tag}/Makefile"]
 
     subprocess.run(subprocess_args, env=get_env())
+
+    if with_initial_setup and os.path.isfile(f"noetic/{tag}/initial-setup.sh"):
+        subprocess.run([f"noetic/{tag}/initial-setup.sh"])
 
 
 if __name__ == "__main__":
@@ -29,7 +33,12 @@ if __name__ == "__main__":
         choices=available_tags(),
         help=f"One of {available_tags()}",
     )
+    argparser.add_argument(
+        "--with-initial-setup",
+        action="store_true",
+        help="Clone and build initial setup packages. (Not applicable to all tags)"
+    )
 
     args = argparser.parse_args()
 
-    build(args.tag)
+    build(args.tag, args.with_initial_setup)
