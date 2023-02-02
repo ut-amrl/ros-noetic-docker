4d3600debe1214dad360973c0b0cd08d3ae5fcfb
Elvin Yang <eyang@cs.utexas.edu>
2023-02-02 02:38:41 -0600

    Print full argparse help message on error

--- launch.py
+++ launch.py
@@ -2,13 +2,21 @@
 
 import argparse
 import subprocess
+import sys
+from typing import NoReturn
 
-from noetic.env import available_tags, get_env, _get_container_user
+from noetic.env import _get_container_user, available_tags, get_env
 
 # TODO: auto-detect which tag to use
 # docker images --filter "reference=$USER*" --format '{{json .}}'
 
 # TODO: auto build the image if necessary
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message: str) -> NoReturn:
+        self.print_help(sys.stderr)
+        self.exit(2, f"\n{self.prog}: error: {message}\n")
 
 
 def launch(tag: str) -> None:
@@ -27,8 +35,14 @@ def launch(tag: str) -> None:
 
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("tag", type=str, choices=available_tags())
+    argparser = ArgumentParser()
+    argparser.add_argument(
+        "tag",
+        type=str,
+        metavar="TAG",
+        choices=available_tags(),
+        help=f"One of {available_tags()}",
+    )
 
     args = argparser.parse_args()
 
